6b12edd0776fe47afb19c25046758c1627552d8a
Gustavo Caravajal <Gustavocarvajal_2001@outlook.es>
2023-02-28 08:28:29 -0300

    presentacion de la api

--- library/settings.py
+++ library/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'library.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['index/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- library/urls.py
+++ library/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('', include('index.urls')),
     path('admin/', admin.site.urls),
     path('api/v1/authors',include('authors.urls')),
     path('api/v2/authors',include('authors.urls')),
